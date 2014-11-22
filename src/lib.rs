ff7250bf233d504af7575c7639646bc7ade9ed59
Jason Duffy <jasonsduffy@gmail.com>
2014-11-22 13:30:29 -0500

    default armor class

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::default::Default;
+
 #[deriving(PartialEq)]
 enum Alignment {
     Good,
@@ -7,18 +9,30 @@ enum Alignment {
 
 struct Character {
     name: String,
-    alignment: Alignment
+    alignment: Alignment,
+    armor_class: int
+}
+
+impl Default for Character {
+    fn default() -> Character {
+        Character {
+            name: "".to_string(),
+            alignment: Evil,
+            armor_class: 10
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::default::Default;
     use super::{Good, Neutral, Evil, Character};
 
     #[test]
     fn test_character_has_a_name() {
         let krusk = Character {
             name: "Krusk".to_string(),
-            alignment: Neutral
+            ..Default::default()
         };
 
         assert_eq!("Krusk".to_string(), krusk.name);
@@ -28,7 +42,8 @@ mod tests {
     fn test_character_alignment_good() {
         let gimble = Character {
             name: "Gimble".to_string(),
-            alignment: Good
+            alignment: Good,
+            ..Default::default()
         };
 
         assert!(Good == gimble.alignment);
@@ -38,7 +53,8 @@ mod tests {
     fn test_character_alignment_neutral() {
         let jozan = Character {
             name: "Jozan".to_string(),
-            alignment: Neutral
+            alignment: Neutral,
+            ..Default::default()
         };
 
         assert!(Neutral == jozan.alignment);
@@ -48,9 +64,21 @@ mod tests {
     fn test_character_alignment_evil() {
         let vadania = Character {
             name: "Vadania".to_string(),
-            alignment: Evil
+            alignment: Evil,
+            ..Default::default()
         };
 
         assert!(Evil == vadania.alignment);
+    }
+
+    #[test]
+    fn test_character_default_armor_class_is_10() {
+        let tordek = Character {
+            name: "Tordek".to_string(),
+            alignment: Neutral,
+            ..Default::default()
+        };
+
+        assert_eq!(10i, tordek.armor_class);
     }
 }
