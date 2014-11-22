2ca616d0f51c4ce4f54fabfd0957416029272095
Jason Duffy <jasonsduffy@gmail.com>
2014-11-22 11:42:46 -0500

    Alignment

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,55 @@
+#[deriving(PartialEq)]
+enum Alignment {
+    Good,
+    Neutral,
+    Evil
+}
+
 struct Character {
     name: String,
+    alignment: Alignment
 }
 
 #[cfg(test)]
-mod test {
-    use super::Character;
+#[test]
+fn test_character_has_a_name() {
+    let krusk = Character {
+        name: "Krusk".to_string(),
+        alignment: Neutral
+    };
 
-    #[test]
-    fn test_character_has_a_name() {
-        let krusk = Character { name: "Krusk".to_string() };
+    assert_eq!("Krusk".to_string(), krusk.name);
+}
 
-        assert_eq!("Krusk".to_string(), krusk.name);
-    }
+#[cfg(test)]
+#[test]
+fn test_character_alignment_good() {
+    let gimble = Character {
+        name: "Gimble".to_string(),
+        alignment: Good
+    };
+
+    assert!(Good == gimble.alignment);
+}
+
+#[cfg(test)]
+#[test]
+fn test_character_alignment_neutral() {
+    let jozan = Character {
+        name: "Jozan".to_string(),
+        alignment: Neutral
+    };
+
+    assert!(Neutral == jozan.alignment);
+}
+
+#[cfg(test)]
+#[test]
+fn test_character_alignment_evil() {
+    let vadania = Character {
+        name: "Vadania".to_string(),
+        alignment: Evil
+    };
+
+    assert!(Evil == vadania.alignment);
 }
